62bd2a92326d4f39d4fd18cd9fd4d8c79cf4ade3
rcallaby <rcallaby@gmail.com>
2018-07-30 15:00:35 -0500

    updated files

---
+++ C# Example/BlockChain.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RudamentaryBlock{
+
+    class BlockChain{
+
+        
+    }
+
+}
---
+++ C# Example/RudimentaryBlock.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* This is going to be the main file that will contain what I consider to be a very rudamentary and simple example of how to write
+a blockchain in C# */
+
+namespace RudamentaryBlock{
+
+    class Program{
+
+       static void Main(string[] args){
+           
+       }
+
+    }
+
+
+}
