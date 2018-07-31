a2264a6ad85240af2bb34d9fc358b9c6642b762b
rcallaby <rcallaby@gmail.com>
2018-07-31 12:32:02 -0500

    updated files and added an MIT License

--- C# Example/BlockChain.cs
+++ C# Example/BlockChain.cs
@@ -7,9 +7,59 @@ using System.Threading.Tasks;
 
 namespace RudamentaryBlock{
 
-    class BlockChain{
+     class Blockchain
+    {
+        // a chain has many blocks
+        List<Block> _chain;
+        public int Difficulty { get; set; }        
 
-        
-    }
+        public Blockchain()
+        {
+            this._chain = new List<Block>();
+            this._chain.Add(CreateGenesisBlock());
+            this.Difficulty = 2;
+        }
+
+        Block CreateGenesisBlock()
+        {
+            return new Block(0, "01/12/2017", "Genesis block", "");
+        }
+
+        public Block GetLatestBlock()
+        {
+            return this._chain.Last();
+        }
+
+        // Once a block is mined, add it to the block
+        public void AddBlock(Block newBlock)
+        {
+            newBlock.PreviousHash = this.GetLatestBlock().Hash;
+            newBlock.Mine(this.Difficulty);
+
+            this._chain.Add(newBlock);
+        }
+
+        public void ValidateChain()
+        {
+
+            for (var i = 1; i < this._chain.Count; i++)
+            {
+                var currentBlock = this._chain[i];
+                var previousBlock = this._chain[i - 1];
+
+                // Check if the current block hash is consistent with the hash calculated
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    throw new Exception("Chain is not valid! Current hash is incorrect!");
+                }
+
+                // Check if the Previous hash match the hash of previous block
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    throw new Exception("Chain is not valid! PreviousHash isn't pointing to the previous block's hash!");
+                }
+            }            
+        }
+}
 
 }
--- C# Example/RudimentaryBlock.cs
+++ C# Example/RudimentaryBlock.cs
@@ -7,17 +7,33 @@ using System.Threading.Tasks;
 
 
 /* This is going to be the main file that will contain what I consider to be a very rudamentary and simple example of how to write
-a blockchain in C# */
+a blockchain in C# 
+
+I was going to do my own implementation of a blockchain yet I found one already on github that I really liked so all credit really belongs to 
+Ray Lu who holds the copyright to this code. Please be sure to check out the LICENSE file to see the conditions of this code under the MIT License.
+
+
+*/
 
 namespace RudamentaryBlock{
 
-    class Program{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCoinBlockchain = new Blockchain();
+            myCoinBlockchain.Difficulty = 3;
 
-       static void Main(string[] args){
-           
-       }
+            Console.WriteLine("Mining a block...");
+            myCoinBlockchain.AddBlock(new Block(1, "03/12/2017", "300"));
 
-    }
+          
+            myCoinBlockchain.ValidateChain();
+            
 
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+}
 
 }
